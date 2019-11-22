357aff02c8354dfad6b44451fd6d39a6a0fa7de6
GGeeli <36232490+GGeeli@users.noreply.github.com>
2019-11-23 00:00:26 +0200

    1.Disabled title bar for project. 2.Added Frame layout to main activity. 3.Created navigation in-between Login & Register fragments.

--- app/src/main/java/com/example/scrumproject/Register.java
+++ app/src/main/java/com/example/scrumproject/Register.java
@@ -4,28 +4,32 @@ package com.example.scrumproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Register extends Fragment {
 
-
-    public Register() {
-        // Required empty public constructor
-    }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+         View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        Button createAcc = view.findViewById(R.id.button);
+
+        createAcc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FragmentTransaction fr=getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new Login());
+                fr.commit();
+            }
+        });
+
+         return view;
     }
 
 }
