a419880e3a217f63f4d87e3cc1063f779627ef04
GGeeli <36232490+GGeeli@users.noreply.github.com>
2019-12-05 08:36:44 +0200

    Finalized navigation drawer with displayable contents: [All users] -login -register [Admin] -create group -create question -join group -view question [Basic user] -join group -join question
    Added vector assets.
    Added header for nav drawer.
    Created sytles,string,dimens xml for files

--- app/src/main/java/com/example/scrumproject/Login.java
+++ app/src/main/java/com/example/scrumproject/Login.java
@@ -2,31 +2,26 @@ package com.example.scrumproject;
 
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class Login extends Fragment {
 
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-       View view = inflater.inflate(R.layout.fragment_login, container, false);
-       Button btn_login = view.findViewById(R.id.btn_register);
-
-        btn_login.setOnClickListener(new View.OnClickListener(){
-            @Override
-                    public void onClick(View v){
-                FragmentTransaction fr=getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container, new Register());
-                fr.commit();
-            }
-        });
-       return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
 }
---
+++ app/src/main/java/com/example/scrumproject/NavigationDrawerActivity.java
@@ -0,0 +1,15 @@
+package com.example.scrumproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+    public class NavigationDrawerActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_nav_drawer);
+        }
+    }
